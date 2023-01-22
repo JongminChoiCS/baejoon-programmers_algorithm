f21f98b92aefc31e9b9266d5084791326a6b5df5
Jongmin Choi <joeychoi7624@gmail.com>
2023-01-22 12:53:43 -0800

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -4,16 +4,10 @@
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
+    int len = s.length();
     
-    if(s.size() % 2 == 1)
-    {
-        answer = s[s.size() / 2];
-    } else
-    {
-        answer = s[s.size() / 2 - 1];
-        answer += s[s.size() / 2];
-    }
-    
-    return answer;
+    if(len % 2 == 0)
+        return s.substr(len / 2 - 1, 2);
+    else 
+        return s.substr(len / 2, 1);
 }
